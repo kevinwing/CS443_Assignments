57a8d92432e14f5435323abd03a3bff79f7617a1
Kevin Wing <kevinrwing@gmail.com>
2023-10-21 23:10:55 -0700

    Updated main.cpp

--- DebouncingSwitchTimeDivMux/src/main.cpp
+++ DebouncingSwitchTimeDivMux/src/main.cpp
@@ -53,24 +53,128 @@
  * @date 2023-10-09
  */
 #include <Arduino.h>
+// #include "switch.hpp"
+
+// type aliases
+using TickType = unsigned long; // type alias for unsigned long
+
+// global constants
+const TickType TickLength = 1000;     // 1 ms
+const TickType DebounceDelay = 50000; // 50 ms
+
+// global variables
+// bool incrementSwitchPressed = false; // flag for increment switch
+// bool decrementSwitchPressed = false; // flag for decrement switch
+// uint8_t currentPin = 0;
+// bool SwitchState;
+byte LastSwitchState = 1;
+bool ButtonPressed = false;
+TickType count = 0; // count of button presses
+TickType lastDebounceTime = 0;
 
 // function prototypes
+void delayMicros(TickType delay);
+bool debounceSwitch(uint8_t pin);
+void displayDigit(uint8_t digit, uint8_t upPin, uint8_t downPin);
 
 void setup()
 {
-  // clear PORTB[0:1] and never set these bits afterwards.
-  DDRB &= ~(1 << DDB0);
+    // ensure 7-segment display is off
+    DDRB &= ~(1 << 0); // clear DDRB[0]
+    DDRB &= ~(1 << 1); // clear DDRB[1]
+
+    // clear PORTB[0:1] and never set these bits afterwards.
+    PORTB &= ~(1 << 0); // clear PORTB[0]
+    PORTB &= ~(1 << 1); // clear PORTB[1]
+
+    // set PORTB[2:3] as input for switches
+    PORTB |= (1 << 2);  // set PORTB[2] as input
+    PORTB |= (1 << 3);  // set PORTB[3] as input
+
+    DDRD = 0b11111111; // set PORTD[0:7] as output for 7-segment display
+
+    Serial.begin(9600);
 }
 
 void loop()
 {
-  // set DDRB[0] or DDRB[1] to turn on a given digit; clear DDRB[0] or DDRB[1] to turn off a given digit.
-  DDRB |= (1 << DDB0);
-  DDRB &= ~(1 << DDB0);
+    // debounce increment switch and increment when debounced
+    if (debounceSwitch(2))
+    {
+        ++count;
+    }
+    else if (debounceSwitch(3))
+    {
+        --count;
+    }
+
+    // display digits
+    displayDigit(count / 10, 0, 1); // display tens digit
+    delayMicros(TickLength);        // delay 1 second
+    displayDigit(count % 10, 1, 0); // display ones digit
+    delayMicros(TickLength);        // delay 1 second
 }
 
-// put function definitions here:
-int myFunction(int x, int y)
+// custom delay function using micros()
+void delayMicros(TickType delay)
 {
-  return x + y;
+    volatile unsigned long start = micros();
+    while (micros() - start < delay)
+    {
+    }
+}
+
+// function to display digit on 7-segment display
+void displayDigit(uint8_t digit, uint8_t upPin, uint8_t downPin)
+{
+    // decimal font table
+    static uint8_t fontTable[] = {
+        0b00111111, // 0
+        0b00000110, // 1
+        0b01011011, // 2
+        0b01001111, // 3
+        0b01100110, // 4
+        0b01101101, // 5
+        0b01111101, // 6
+        0b00000111, // 7
+        0b01111111, // 8
+        0b01101111, // 9
+    };
+
+    // turn off downPin
+    DDRB &= ~(1 << downPin);
+
+    // turn on upPin
+    DDRB |= (1 << upPin);
+
+    // set upPin digit to digit for display
+    PORTD = fontTable[digit];
+}
+
+// function to debounce switch using micros()
+bool debounceSwitch(uint8_t pin)
+{
+    // read state of pin into variable
+    byte currentSwitchState = (PINB & (1 << pin)) >> pin;
+
+    if (currentSwitchState != LastSwitchState)
+    {
+        lastDebounceTime = micros();
+    }
+
+    if ((micros() - lastDebounceTime) > DebounceDelay)
+    {
+        if (currentSwitchState == 0 && !ButtonPressed)
+        {
+            ButtonPressed = true;
+            LastSwitchState = currentSwitchState;
+            return true;
+        }
+        else if (currentSwitchState == 1)
+        {
+            ButtonPressed = false;
+            LastSwitchState = currentSwitchState;
+        }
+    }
+    return false;
 }
