0560ad714c584dda0f10e39ce3c36350a76f597a
Kevin Wing <kevinrwing@gmail.com>
2023-11-01 15:15:15 -0700

    improved debouncer class

--- PWMAssignment/include/debouncer.hpp
+++ PWMAssignment/include/debouncer.hpp
@@ -3,34 +3,65 @@
 
 #include <Arduino.h>
 
+using TickType = unsigned long;
+
+const TickType debounceDelay = 1000; // the debounce time
+
 /**
- * @brief Debounce a digital input signal
+ * @brief Class for debouncing a digital input
  * 
  */
 class Debouncer {
     private:
-        uint8_t btnPin;
-        uint16_t state;
+        uint8_t btnPin; // pin number of button
+        uint8_t state; // current state of button
+        uint8_t lastState; // previous state of button
+        TickType lastDebounceTime; // last time button state changed
     
     public:
-        void begin(uint8_t button)
+        /**
+         * @brief perform some setup
+         * 
+         */
+        void begin(uint8_t btn)
         {
-            btnPin = button;
-            state = 0;
+            btnPin = btn;
+            state = HIGH;
+            lastState = HIGH;
             // set pin to input
             DDRC &= ~(1 << btnPin);
         }
 
         /**
-         * @brief debounce algrorithm modified from Jack Ganssle, http://www.ganssle.com/debouncing-pt2.htm
+         * @brief debounce switch input signal and return true if pressed
          * 
          * @return true 
          * @return false 
          */
         bool debounce()
         {
-            state = ((state << 1) | ((PINC >> btnPin) & 1) | 0xe000);
-            return (state == 0xf000);
+            uint8_t tempState = PINC & (1 << btnPin); // read input pin
+
+            if (tempState != lastState)
+            {
+                lastDebounceTime = micros();
+            }
+
+            if ((micros() - lastDebounceTime) > debounceDelay)
+            {
+                if (tempState != state)
+                {
+                    state = tempState;
+                    if (state == LOW)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            lastState = tempState;
+
+            return false;
         }
 };
 
